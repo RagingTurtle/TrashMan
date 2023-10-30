59274ccf46918f90a53a0bf03709ef4066d631c9
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-10-30 11:51:35 -0500

    bbcode errorfix

--- BBCode/bbcode_uppercase.gd
+++ BBCode/bbcode_uppercase.gd
@@ -12,7 +12,7 @@ func get_text_server():
 func _process_custom_fx(char_fx):
 
 	var value = char_fx.glyph_index
-	var char = char(get_text_server().font_get_char_from_glyph_index(char_fx.font, 1, value)).to_upper()
-	char_fx.glyph_index = get_text_server().font_get_glyph_index(char_fx.font, 1, char.unicode_at(0), 0)
+	var character = char(get_text_server().font_get_char_from_glyph_index(char_fx.font, 1, value)).to_upper()
+	char_fx.glyph_index = get_text_server().font_get_glyph_index(char_fx.font, 1, character.unicode_at(0), 0)
 	
 	return true
