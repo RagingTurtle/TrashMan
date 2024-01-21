46ebecde01f9c3788a464f2fb3ecacd6718473f1
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-01-20 21:35:27 -0600

    Options Screen

--- Menus/options_menu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-var caller
-
-func _on_back_pressed() -> void:
-	self.hide()
-	#caller.show()
--- scripts/screens.gd
+++ scripts/screens.gd
@@ -2,8 +2,10 @@ extends CanvasLayer
 
 @onready var title_screen: Screen = $TitleScreen
 @onready var credits_screen: Screen = $CreditsScreen
+@onready var options_screen: Control = $OptionsScreen
 
 var active_screen: Screen = null
+var last_screen: Screen = null
 
 func _ready() -> void:
 	register_buttons()
@@ -21,19 +23,22 @@ func _on_button_pressed(button: Node) -> void:
 		"TitlePlay":
 			print(button.name)
 		"TitleOptions":
-			print(button.name)
+			change_screen(options_screen)
 		"TitleCredits":
 			change_screen(credits_screen)
 		"TitleQuit":
 			get_tree().quit()
 		"CreditsBack":
-			change_screen(title_screen)
+			change_screen(last_screen)
+		"OptionsBack":
+			change_screen(last_screen)
 
 				
 func change_screen(new_screen: Screen) -> void:
 	if active_screen:
 		var disappear_tween:Tween = active_screen.disappear()
 		await(disappear_tween.finished)
+	last_screen = active_screen
 	active_screen = new_screen
 	if active_screen:
 		var appear_tween:Tween = active_screen.appear()
