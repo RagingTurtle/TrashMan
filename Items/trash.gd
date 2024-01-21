2dfd9f9090b121c91a7c22e50d0679d64522d9c8
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-01-21 16:29:14 -0600

    prune old scenes

--- Items/trash.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@onready var sprite: Sprite2D = $Sprite2D
-
-signal pick_up_trash(Area2d)
-
-func _on_body_entered(body: Node2D) -> void:
-	if (body.name) == "Player":
-		emit_signal("pick_up_trash", self)
